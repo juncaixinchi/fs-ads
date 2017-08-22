b67d05346e790499cec6227261edf4ba949b5088
juncaixinchi <juncaixinchi111@gmail.com>
2017-08-22 21:18:08 +0800

    add test

--- test.js
+++ test.js
@@ -1,20 +1,70 @@
+const fs =require('fs')
 const ads = require('./index')
+const expect = require('chai').expect
 
-const path = './tmp/a.txt'
+const path = fs.mkdtempSync('/tmp')
+
 const stream = 'user.fs-ads.test'
+const data = 'just a test string'
 
-const ss = ads.setSync(path, stream, 'just a test string')
-const gs = ads.getSync(path, stream)
-const rs = ads.removeSync(path, stream)
-console.log('ss gs rs', ss, gs, rs)
+describe('fs-ads #sync', () => {
+  it('should throw ENOENT Error', done => {
+    try {
+      ads.getSync(path, stream)
+    } catch (e) {
+      if (e && e.code === 'ENOENT') done()
+      throw new Error(e)        
+    }
+    throw new Error('remove ads failed !')        
+  })
 
-ads.set(path, stream, 'just a test string', (error) => {
-  console.log('ads.set, error:', error)
-  ads.get(path, stream, (error, value) => {
-    console.log('ads.get, error:', error, ', value:', value)
-    ads.remove(path, stream, (error) => {
-      console.log('ads.remove, error:', error)
-    })
+  it('should set an ads', () => {
+    ads.setSync(path, stream, data)
+  })
+
+  it('should get an ads', () => {
+    expect(ads.getSync(path, stream)).to.be.equal(data)
+  })
+
+  it('should remove an ads', () => {
+    ads.removeSync(path, stream)
+  })
+
+  it('should throw ENOENT Error', done => {
+    try {
+      ads.getSync(path, stream)
+    } catch (e) {
+      if (e && e.code === 'ENOENT') done()
+      throw new Error(e)        
+    }
+    throw new Error('remove ads failed !')        
   })
 })
 
+describe('fs-ads #async', () => {
+  it('should throw ENOENT Error', done => {
+      ads.get(path, stream, (error) => {
+        if (error && error.code === 'ENOENT') done()
+        throw new Error(error)        
+      })
+  })
+
+  it('should set an ads', () => {
+    ads.set(path, stream, data, (error) => {
+      if (error) throw done(error)
+    })
+  })
+
+  it('should get an ads', () => {
+    ads.get(path, stream, (error, result) => {
+      if (error) throw done(error)
+      expect(result).to.be.equal(data)
+    })
+  })
+
+  it('should remove an ads', () => {
+    ads.remove(path, stream, (error, result) => {
+      if (error) throw done(error)
+    })
+  })
+})
